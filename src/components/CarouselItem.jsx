93cb9c8b873dedb3e34a998af2d9214cd1a15baa
David Vargas Hernandez <david-0610@live.com.mx>
2020-08-08 18:08:22 -0500

    Fake API implementada con react hooks

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,19 +3,21 @@ import '../assets/styles/components/CarouselItem.scss';
 import play from '../assets/static/play-icon.png';
 import plus from '../assets/static/plus-icon.png';
 
-const itemTitle = "Título descriptivo";
-const itemSubtitle = "2019 16+ 114 minutos";
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/1172207/pexels-photo-1172207.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&w=500" alt=""  />
+        <img className="carousel-item__img" src={cover} alt={title}  />
         <div className="carousel-item__details">
           <div>
             <img className="carousel-item__details--img" src={play} alt="Play Icon"/> 
             <img className="carousel-item__details--img" src={plus} alt="Plus Icon"/> 
           </div>
-          <p className="carousel-item__details--title">{itemTitle}</p>
-          <p className="carousel-item__details--subtitle">{itemSubtitle}</p>
+          <p className="carousel-item__details--title">{title}</p>
+          <p className="carousel-item__details--subtitle">
+          {
+            `${year} ${contentRating} ${duration} minuts`
+          }
+          </p>
         </div>
     </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header'
 import '../assets/styles/App.scss'
 import Search from '../components/Search';
@@ -7,38 +7,56 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer'
 
-const App = () => (
-    <div className="App">￼￼￼
-        <Header />
-        <Search />
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Originales">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-);
+const App = () => {
+    const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: []});
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+          try {
+            const response = await fetch("http://localhost:3000/initialState");
+            const data = await response.json();
+            return setVideos(data);
+          } catch {
+            console.log(error);
+          }
+        };
+        fetchVideos();
+      }, []);
+
+    console.log(videos);
+
+    return(
+        <div className="App">￼￼￼
+            <Header />
+            <Search />
+
+            {videos.mylist.length > 0 &&
+                <Categories title="Mi lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
+
+            <Categories title="Tendencias">
+                <Carousel>
+                {videos.trends.map(item => 
+                    <CarouselItem key={item.id} {...item} />
+                )}          
+                </Carousel>
+            </Categories>
+            <Categories title="Originales">
+                <Carousel>
+                {
+                    videos.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )
+                }
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
