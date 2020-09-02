b188fc655babf9aee5ff6741fcf36668a528d81a
David Vargas Hernandez <david-0610@live.com.mx>
2020-09-02 01:51:28 -0500

    Implementando register con redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,71 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions/index';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png'
 
-const Header = () => (
+const Header = props => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({})
+  }
+
+  return(
     <header className="header">
       <Link to="/">
         <img className="header__img" src={logo} alt="Platzi Video"/>
       </Link>
     <div className="header__menu">
       <div className="header__menu--profile">
-        <img src={userIcon} alt=""/>
+        {
+          hasUser ? 
+            <img src={gravatar(user.email)} alt={user.email} /> 
+            :
+            <img src={userIcon} alt=""/>
+        }
         <p>Perfil</p>
       </div>
       <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><Link to="/login">Iniciar sesión</Link></li>
+        {
+          hasUser ? 
+            <li><a href="/">{user.name}</a></li> 
+            : 
+            null
+        }
+        {
+          hasUser ? 
+            <li>
+              <a href="#logout" onClick={handleLogout}>Cerrar sesión</a>
+            </li>
+          :
+          <li>
+            <Link to="/login">
+              Iniciar sesión
+            </Link>
+          </li>
+          
+        }
+      
+        
       </ul>
     </div>
-  </header>
-);
+    </header>
+  );
+}
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
